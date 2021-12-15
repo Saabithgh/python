2c1312d4b30dc6d2e47b37589c077cbac58bff19
Sayeth Saabith <60929890+Saabithgh@users.noreply.github.com>
2021-12-15 20:10:40 +0530

    Add files via upload

---
+++ First Program.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 17 00:00:20 2021
+
+@author: ALSS
+"""
+
+#print("Enter Your Name: ")
+#name = input()
+#print("Your Name is ",name)
+# name = input("Enter Your name: ")
+# print("Your Good Name is ", name)
+
+# num1 = int(input("Enter Your number: "))
+# print("You Entered ", num1+6)
+
+# num1 = input("Enter Your number: ")
+# print("You Entered ", int(num1)+6)
+
+# #num1 = input("Enter Your number: ")
+# #print("You Entered ", num1+6)
+
+
+# first_num = input("Enter Your First Number: ")
+# second_num = input("Enter Your Second Number: ")
+# print("Sum of Two number is: ",int(first_num)+int(second_num))
+# print("Sub of Two number is: ",int(first_num)-int(second_num))
+# print("Mult of Two number is: ",int(first_num)*int(second_num))
+# print("Div of Two number is: ",int(first_num)/int(second_num))
+
+'''String Slicing......................................'''
+product = "asus rog | Rs.100000 | new"
+print(product.upper())
+print(product.index("R"))
+print(product.split("|"))
+print(product[:product.index("|")])
